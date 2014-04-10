439688275589f9fa1932e106b980f4df236cf39c
Med Amine Ben Asker <ben.asker.amine@gmail.com>
2014-04-11 01:25:15 +0200

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -8,62 +8,54 @@
 }
 
    
-   $action = isset($_POST['to'])
+$action = isset($_POST['to'])
 				AND isset($_POST['from'])
 				AND isset($_POST['message'])
 				AND isset($_POST['subject']); 
    
    
 
-	/**
-	*	INITIALISATION
-	*/
-			$errorMessage = "";
-			$succMessage  = "";
+
+//	INITIALISATION
+$errorMessage = "";
+$succMessage  = "";
 
 
-			if ($action)
-		{
+if ($action)
+{
 	
-    $mails = explode(';', $_POST['to']);
+      $mails = explode(';', $_POST['to']);
     $subject = $_POST['subject'];
     $message = $_POST['message'];
-	$header = "From:".$_POST['from']." \r\n";
+     $header = "From:".$_POST['from']." \r\n";
 	
-	   foreach($mails as $to){
-		if (email_valid($to)){
-		//$resp = mail($to,$subject,$message,$header);
-		$resp = true;
-	
-			if( $resp)  
-			$succMessage.="Message sent successfully to ".$to."\n";
-			else
-			$errorMessage.= "Message could not be sent to ".$to."\n";
-		}
-		else
-		$errorMessage.= $to." isn't a valid adress mail \n";
-		}
+    foreach($mails as $to){
+       if (email_valid($to)){
+          $resp = mail($to,$subject,$message,$header);
+             if( $resp)  
+                $succMessage.="Message sent successfully to ".$to."\n";
+              else
+                $errorMessage.= "Message could not be sent to ".$to."\n";
+        }
+        else
+           $errorMessage.= $to." isn't a valid adress mail \n";
+         }
 		
-	// JSON out put 
-	
-	
-	$contentToJSON = array("to" => $mails,
-					  "subject" => $subject,
-				         "from" => $_POST['from'],
-				      "message" => $message,
-				          "log" => $succMessage.$errorMessage);
+ // JSON out put 
+$contentToJSON = array("to" => $mails,
+                   "subject" => $subject,
+                      "from" => $_POST['from'],
+                   "message" => $message,
+                       "log" => $succMessage.$errorMessage);
 				
-	$tojson = json_encode($contentToJSON);
-	
-	file_put_contents("logs/js".
-							md5($contentToJSON['message']).
-							"_".
-							time().
-							".json"
-						,
-					  $tojson);
+$tojson = json_encode($contentToJSON);
+file_put_contents("logs/js".
+                          md5($contentToJSON['message']).
+                          "_".
+                          time().
+                          ".json"
+                           ,
+                  $tojson);
 
-	
-	
-   } // end action 
+} 
 ?>
